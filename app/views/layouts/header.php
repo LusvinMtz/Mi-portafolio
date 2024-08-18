87e6b0dfbdcd7404eee49a28e1be83eb00ee6c9d
LusvinMtz <lmartinezm20@miumg.com.gt>
2024-08-18 10:47:05 -0600

    second commit

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -29,9 +29,9 @@
     <header class="masthead bg-primary text-white text-center">
         <div class="container d-flex align-items-center flex-column">
             <!-- Masthead Avatar Image-->
-            <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="..." style="margin-top: 75px;" />
+            <img class="masthead-avatar mb-5" src="assets/img/lmtz.png" width="200" alt="..." style="margin-top: 75px; border:20px" />
             <!-- Masthead Heading-->
-            <h1 class="masthead-heading text-uppercase mb-0">Start Bootstrap</h1>
+            <h1 class="masthead-heading text-uppercase mb-0">Lusvin Mtz</h1>
             <!-- Icon Divider-->
             <div class="divider-custom divider-light">
                 <div class="divider-custom-line"></div>
@@ -39,6 +39,6 @@
                 <div class="divider-custom-line"></div>
             </div>
             <!-- Masthead Subheading-->
-            <p class="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p>
+            <p class="masthead-subheading font-weight-light mb-0">- Web Designer -</p>
         </div>
     </header>
